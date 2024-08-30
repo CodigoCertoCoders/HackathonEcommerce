22bc29ffb35b36d7952fbeeb18617cbcb538f1f9
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-08-30 14:38:49 -0300

    :sparkles: 	feat: adicionando cors e refatorando o codigo

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,66 @@
-from fastapi import FastAPI
-from routes import produtos_router, auth_router, pedidos_router, usuarios_router, categorias_router, admin_router
+import logging
+import os
 
-import jwt
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import (
+    produtos_router,
+    auth_router,
+    pedidos_router,
+    usuarios_router,
+    categorias_router,
+    admin_router,
+)
+from starlette.responses import JSONResponse
+
+# Criar pasta de logs se não existir
+os.makedirs("logs", exist_ok=True)
+
+# Configuração do logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("logs/app.log"),
+        logging.StreamHandler()  # Para visualizar os logs no console
+    ]
+)
 
 app = FastAPI(title="Pizzaria Hackaton")
 
-app.include_router(usuarios_router, prefix='/usuarios', tags=['Usuários'])
-app.include_router(categorias_router, prefix='/categorias', tags=['Categorias'])
-app.include_router(produtos_router, prefix='/produtos', tags=['Produtos'])
-app.include_router(pedidos_router, prefix='/pedidos', tags=['Pedidos'])
-app.include_router(auth_router, prefix='/auth', tags=['Auth'])
-app.include_router(admin_router, prefix='/admin', tags=['Admin'])
+# Configuração de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # podemos adicicionar a rota do front dps
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Incluir os routers com prefixos e tags
+routers = [
+    (usuarios_router, '/usuarios', ['Usuários']),
+    (categorias_router, '/categorias', ['Categorias']),
+    (produtos_router, '/produtos', ['Produtos']),
+    (pedidos_router, '/pedidos', ['Pedidos']),
+    (auth_router, '/auth', ['Auth']),
+    (admin_router, '/admin', ['Admin']),
+]
+
+for router, prefix, tags in routers:
+    app.include_router(router, prefix=prefix, tags=tags)
 
 @app.get('/')
-async def index():
-    return {
-        "Pizzaria": "Hackaton"
-    }
+async def index() -> dict:
+    """Endpoint principal da aplicação."""
+    return {"Pizzaria": "Hackaton"}
 
+# Middleware de tratamento de erros
+@app.exception_handler(Exception)
+async def global_exception_handler(request, exc):
+    logging.error(f"Erro inesperado: {exc}")
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Ocorreu um erro inesperado. Tente novamente mais tarde."},
+    )

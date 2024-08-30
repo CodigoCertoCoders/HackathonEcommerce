14023fc6d00a32ea03a0b70b3bc3fa721d1600fd
Namibo06 <namibo654@gmail.com>
2024-08-30 14:58:01 -0300

    feat: client controller, already with methods to search for client and create client working

---
+++ Backend/src/main/java/com/guardians_of_the_code/controllers/ClientController.java
@@ -0,0 +1,46 @@
+package com.guardians_of_the_code.controllers;
+
+import com.guardians_of_the_code.dtos.ClientRequestDTO;
+import com.guardians_of_the_code.dtos.ClientResponseDTO;
+import com.guardians_of_the_code.dtos.MessageStatusDTO;
+import com.guardians_of_the_code.entities.Client;
+import com.guardians_of_the_code.interfaces.JPAInterface;
+import com.guardians_of_the_code.use_cases.CreateClientUseCase;
+import com.guardians_of_the_code.use_cases.FindClientByUUIDUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+    @Autowired
+    private FindClientByUUIDUseCase findClientByUUIDUseCase;
+
+    @Autowired
+    private CreateClientUseCase createClientUseCase;
+
+    @Autowired
+    private JPAInterface repository;
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<ClientResponseDTO> getClientById(@PathVariable UUID uuid){
+        ClientResponseDTO clientResponseDTO = findClientByUUIDUseCase.execute(uuid);
+        return ResponseEntity.ok(clientResponseDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<MessageStatusDTO> createClient(@RequestBody ClientRequestDTO client, UriComponentsBuilder uriBuilder){
+        Client clientModel = createClientUseCase.execute(client);
+        UUID clientId = clientModel.getId();
+        URI path = uriBuilder.path("/clients/{id}").buildAndExpand(clientId).toUri();
+
+        MessageStatusDTO response = new MessageStatusDTO("Cliente criado com sucesso",201);
+
+        return ResponseEntity.created(path).body(response);
+    }
+}
---
+++ Backend/src/main/java/com/guardians_of_the_code/use_cases/CreateClientUseCase.java
@@ -0,0 +1,51 @@
+package com.guardians_of_the_code.use_cases;
+
+import com.guardians_of_the_code.dtos.ClientRequestDTO;
+import com.guardians_of_the_code.dtos.MessageStatusDTO;
+import com.guardians_of_the_code.entities.Client;
+import com.guardians_of_the_code.exceptions.HandleBadRequestException;
+import com.guardians_of_the_code.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateClientUseCase {
+    @Autowired
+    private ClientService service;
+
+    public Client execute(ClientRequestDTO client){
+        if(client.getName().isEmpty() || client.getName().isBlank()){
+            throw new HandleBadRequestException("Nome não pode ser vazio ou nulo");
+        }
+
+        if(client.getCep().isEmpty() || client.getCep().isBlank()){
+            throw new HandleBadRequestException("Cep não pode ser vazio ou nulo");
+        }
+
+        if(client.getEmail().isEmpty() || client.getEmail().isBlank()){
+            throw new HandleBadRequestException("Email não pode ser vazio ou nulo");
+        }
+
+        if(client.getPhone().isEmpty() || client.getPhone().isBlank()){
+            throw new HandleBadRequestException("Telefone não pode ser vazio ou nulo");
+        }
+
+        if(client.getCep().length() != 8){
+            throw new HandleBadRequestException("O cep deve seguir o formato 00000000,contendo 8 caracteres");
+        }
+
+        if(client.getName().length() > 30){
+            throw new HandleBadRequestException("O nome deve ter até 30 caracteres");
+        }
+
+        if(client.getEmail().length() > 120){
+            throw new HandleBadRequestException("O email deve ter até 120 caracteres");
+        }
+
+        if(client.getPhone().length() != 11){
+            throw new HandleBadRequestException("O cep deve seguir o formato 99221111111,contendo 11 caracteres");
+        }
+
+        return service.createClient(client);
+    }
+}

f07e0565d7a00b4cee66e1b96826dcc5b48772c6
Namibo06 <namibo654@gmail.com>
2024-08-30 14:51:23 -0300

    feat: interface for clients to be implemented by the repository and service

---
+++ Backend/src/main/java/com/guardians_of_the_code/interfaces/ClientInterface.java
@@ -0,0 +1,17 @@
+package com.guardians_of_the_code.interfaces;
+
+import com.guardians_of_the_code.dtos.ClientRequestDTO;
+import com.guardians_of_the_code.dtos.ClientResponseDTO;
+import com.guardians_of_the_code.dtos.MessageStatusDTO;
+import com.guardians_of_the_code.entities.Client;
+
+import java.util.UUID;
+
+public interface ClientInterface {
+    public ClientResponseDTO findByClient(UUID uuid);
+    public Client createClient(ClientRequestDTO client);
+    public MessageStatusDTO updateClient(UUID uuid,ClientRequestDTO client);
+    public MessageStatusDTO deleteClient(UUID uuid);
+    public boolean existsClientByEmail(String email);
+    public boolean existsClient(UUID uuid);
+}

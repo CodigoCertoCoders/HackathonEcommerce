9d601a7318d7610942025f0f911d8ae6e2927301
Namibo06 <namibo654@gmail.com>
2024-08-31 01:01:57 -0300

    feat: added two more queries by JPA

--- Backend/src/main/java/com/guardians_of_the_code/interfaces/JPAInterface.java
+++ Backend/src/main/java/com/guardians_of_the_code/interfaces/JPAInterface.java
@@ -2,6 +2,8 @@ package com.guardians_of_the_code.interfaces;
 
 import com.guardians_of_the_code.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,8 +12,14 @@ import java.util.UUID;
 @Repository
 public interface JPAInterface extends JpaRepository<Client,UUID> {
     boolean existsClientByEmail(String email);
-    Optional<Client> findClientById(UUID id);
-    boolean existsClientById(UUID uuid);
-    boolean existsClientByPhone(String phone);
-}
 
+    Optional<Client> findClientById(UUID id);
+
+    boolean existsClientById(UUID uuid);
+
+    boolean existsClientByPhone(String phone);
+
+    boolean existsByIdAndEmail(UUID uuid, String email);
+
+    boolean existsByEmailAndIdNot(String email,UUID uuid);
+}

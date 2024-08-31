3544f08282b96fb08b436fc9f62484cf1fd37e35
Namibo06 <namibo654@gmail.com>
2024-08-31 11:42:00 -0300

    Refactor: added validations, same as creating customer

--- Backend/src/main/java/com/guardians_of_the_code/use_cases/UpdateClientUseCase.java
+++ Backend/src/main/java/com/guardians_of_the_code/use_cases/UpdateClientUseCase.java
@@ -2,6 +2,7 @@ package com.guardians_of_the_code.use_cases;
 
 import com.guardians_of_the_code.dtos.ClientRequestDTO;
 import com.guardians_of_the_code.dtos.MessageStatusDTO;
+import com.guardians_of_the_code.exceptions.HandleBadRequestException;
 import com.guardians_of_the_code.exceptions.HandleConflictException;
 import com.guardians_of_the_code.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,48 @@ public class UpdateClientUseCase {
     private ClientService service;
 
     public MessageStatusDTO execute(UUID uuid, ClientRequestDTO request){
-        boolean existsByEmailAndId = service.existsClientByEmailAndId(uuid, request.getEmail());
+        if(request.getName().isEmpty() || request.getName().isBlank()){
+            throw new HandleBadRequestException("Nome não pode ser vazio ou nulo");
+        }
 
+        if(request.getCep().isEmpty() || request.getCep().isBlank()){
+            throw new HandleBadRequestException("Cep não pode ser vazio ou nulo");
+        }
+
+        if(request.getEmail().isEmpty() || request.getEmail().isBlank()){
+            throw new HandleBadRequestException("Email não pode ser vazio ou nulo");
+        }
+
+        if(request.getPhone().isEmpty() || request.getPhone().isBlank()){
+            throw new HandleBadRequestException("Telefone não pode ser vazio ou nulo");
+        }
+
+        if(request.getCep().length() != 8){
+            throw new HandleBadRequestException("O cep deve seguir o formato 00000000,contendo 8 caracteres");
+        }
+
+        if(request.getName().length() > 30 || request.getName().length() < 4){
+            throw new HandleBadRequestException("O nome deve ter no minimo 4 e no máximo 30 caracteres");
+        }
+
+        if(request.getEmail().length() > 120){
+            throw new HandleBadRequestException("O email deve ter até 120 caracteres");
+        }
+
+        if(request.getPhone().length() != 11){
+            throw new HandleBadRequestException("O cep deve seguir o formato 99221111111,contendo 11 caracteres");
+        }
+
+        if(request.getPassword().length() > 100 || request.getPassword().length() < 8){
+            throw new HandleBadRequestException("A senha deve ter no minimo 8 e no máximo 100 caracteres");
+        }
+
+        boolean existsPhone = service.existsClientByPhone(request.getPhone());
+        if(existsPhone){
+            throw new HandleConflictException("Número de celular já existe");
+        }
+
+        boolean existsByEmailAndId = service.existsClientByEmailAndId(uuid, request.getEmail());
         if(existsByEmailAndId){
             throw new HandleConflictException("Email já existe");
         }

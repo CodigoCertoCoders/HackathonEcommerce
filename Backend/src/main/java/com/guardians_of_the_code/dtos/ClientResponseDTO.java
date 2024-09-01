55a0a15c0f5ceae43f08408018b1faf7afc3a143
Namibo06 <namibo654@gmail.com>
2024-09-01 11:05:39 -0300

    feat: removed cep and added state, city, neighborhood, road, number_house and complement in update client

--- Backend/src/main/java/com/guardians_of_the_code/dtos/ClientResponseDTO.java
+++ Backend/src/main/java/com/guardians_of_the_code/dtos/ClientResponseDTO.java
@@ -12,6 +12,11 @@ public class ClientResponseDTO {
     private UUID id;
     private String name;
     private String email;
-    private String cep;
+    private String uf;
+    private String city;
+    private String neighborhood;
+    private String road;
+    private String number_house;
+    private String complement;
     private String phone;
 }

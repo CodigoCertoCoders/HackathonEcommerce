6cd36c14a3f66c1d1db5831579f4b19dc266b779
Namibo06 <namibo654@gmail.com>
2024-09-02 14:41:43 -0300

    feat: DTO for response of many products

---
+++ Backend/src/main/java/com/guardians_of_the_code/dtos/GetAllProductsResponseDTO.java
@@ -0,0 +1,16 @@
+package com.guardians_of_the_code.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class GetAllProductsResponseDTO {
+    private List<ProductResponseDTO> products;
+}

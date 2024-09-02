83e0be892e1f36927c40159369e6632622d48ce3
Namibo06 <namibo654@gmail.com>
2024-09-02 13:48:49 -0300

    refactor: rename class and remove import's

--- Backend/src/main/java/com/guardians_of_the_code/interfaces/JPAInterfaceClient.java
+++ Backend/src/main/java/com/guardians_of_the_code/interfaces/JPAInterfaceClient.java
@@ -2,16 +2,13 @@ package com.guardians_of_the_code.interfaces;
 
 import com.guardians_of_the_code.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface JPAInterface extends JpaRepository<Client,UUID> {
+public interface JPAInterfaceClient extends JpaRepository<Client,UUID> {
     // clients
     boolean existsClientByEmail(String email);
 

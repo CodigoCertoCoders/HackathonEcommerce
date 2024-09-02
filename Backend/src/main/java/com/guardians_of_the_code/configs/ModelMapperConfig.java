528e0a9e0edb3fbd406ec53bd0456ef279a1f25e
Namibo06 <namibo654@gmail.com>
2024-09-02 13:51:31 -0300

    feat: configured modelMapper to map from Product to ProductResponseDTO

--- Backend/src/main/java/com/guardians_of_the_code/configs/ModelMapperConfig.java
+++ Backend/src/main/java/com/guardians_of_the_code/configs/ModelMapperConfig.java
@@ -1,5 +1,8 @@
 package com.guardians_of_the_code.configs;
 
+import com.guardians_of_the_code.dtos.ProductResponseDTO;
+import com.guardians_of_the_code.entities.Product;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.modelmapper.ModelMapper;
@@ -8,6 +11,20 @@ import org.modelmapper.ModelMapper;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addMappings(new PropertyMap<Product, ProductResponseDTO>() {
+            @Override
+            protected void configure() {
+                map().setUuid(source.getId());
+                map().setName(source.getName());
+                map().setUrl(source.getUrl());
+                map().setCategory(source.getCategory());
+                map().setPrice(source.getPrice());
+                map().setDescription(source.getDescription());
+            }
+        });
+
+        return modelMapper;
     }
 }

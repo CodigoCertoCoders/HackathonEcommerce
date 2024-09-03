2a64230e890fa72a8823c61f644adbdc7434e3e1
Namibo06 <namibo654@gmail.com>
2024-09-03 11:47:02 -0300

    refactor: added path

--- Backend/src/main/java/com/guardians_of_the_code/use_cases/GetAllProductsUseCase.java
+++ Backend/src/main/java/com/guardians_of_the_code/use_cases/GetAllProductsUseCase.java
@@ -1,9 +1,11 @@
 package com.guardians_of_the_code.use_cases;
 
 import com.guardians_of_the_code.dtos.GetAllProductsResponseDTO;
+import com.guardians_of_the_code.dtos.ProductResponseDTO;
 import com.guardians_of_the_code.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 public class GetAllProductsUseCase {
@@ -11,6 +13,10 @@ public class GetAllProductsUseCase {
     private ProductService service;
 
     public GetAllProductsResponseDTO execute(){
-        return service.getAllProducts();
+        List<ProductResponseDTO> products = service.getAllProducts();
+
+        String path = "http://localhost:8080/";
+
+        return new GetAllProductsResponseDTO(products,path);
     }
 }

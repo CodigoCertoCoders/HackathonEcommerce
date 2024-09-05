27d08a9568782f7acd13191c6e39427931b141fd
bentoluizv <bentoluizv@gmail.com>
2024-09-04 23:32:15 -0300

    add pizza bad bebidas models

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pizza(models.Model):
+    PizzaType = models.TextChoices('PizzaType', 'DOCE SALGADA')
+    category = models.CharField(choices=PizzaType, max_length=10)  # type: ignore
+    name = models.CharField(max_length=10)
+    price = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Bebida(models.Model):
+    name = models.CharField(max_length=10)
+    price = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.name

0b4e1c7aba3aede8191077d9cf7d00c1c20b98e4
Namibo06 <namibo654@gmail.com>
2024-09-05 14:31:45 -0300

    feat: object mapper

---
+++ Backend/src/main/java/com/guardians_of_the_code/configs/AppConfig.java
@@ -0,0 +1,14 @@
+package com.guardians_of_the_code.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}

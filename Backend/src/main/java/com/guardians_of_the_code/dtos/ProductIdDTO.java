0f0eb38b20a6164b35982d3c3eec8248963028a9
Namibo06 <namibo654@gmail.com>
2024-09-05 14:31:59 -0300

    feat: dto for products

---
+++ Backend/src/main/java/com/guardians_of_the_code/dtos/ProductIdDTO.java
@@ -0,0 +1,18 @@
+package com.guardians_of_the_code.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ProductIdDTO {
+    private String id;
+
+
+}

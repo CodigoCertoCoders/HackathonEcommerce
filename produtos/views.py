1a6b78509f1d52453ce9fb8fa535c72432a0678b
bentoluizv <bentoluizv@gmail.com>
2024-09-04 23:33:25 -0300

    add product list view

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.core.serializers import serialize
+from django.http import JsonResponse
+
+from .models import Bebida, Pizza
 
 # Create your views here.
+
+
+def product_list(request):
+    pizzas = serialize('json', Pizza.objects.all())
+    bebidas = serialize('json', Bebida.objects.all())
+    data = {'products': [{'pizzas': pizzas}, {'bebidas': bebidas}]}
+
+    return JsonResponse(data)

0c4616fd564f4700c5e8d980364beb23a08916ea
bentoluizv <bentoluizv@gmail.com>
2024-09-04 23:47:11 -0300

    add static root

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / STATIC_URL
 
 
 # Default primary key field type

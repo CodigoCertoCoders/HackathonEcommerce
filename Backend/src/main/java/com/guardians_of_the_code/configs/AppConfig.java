abc206d3c3aaac1657329082aad602e8cecc8665
Namibo06 <namibo654@gmail.com>
2024-09-05 15:37:28 -0300

    refactor: changed object mapper

--- Backend/src/main/java/com/guardians_of_the_code/configs/AppConfig.java
+++ Backend/src/main/java/com/guardians_of_the_code/configs/AppConfig.java
@@ -3,12 +3,15 @@ package com.guardians_of_the_code.configs;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }

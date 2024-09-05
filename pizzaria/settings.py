a727f08b0d7f611511bd3b9aaa9524651b9ca26a
bentoluizv <bentoluizv@gmail.com>
2024-09-04 23:42:10 -0300

    remove STATIC_URL

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

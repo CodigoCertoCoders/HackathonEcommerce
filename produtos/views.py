99a8e09fce20f66690769ad60f4e863385726551
bentoluizv <bentoluizv@gmail.com>
2024-09-05 10:54:02 -0300

    add data to view

--- produtos/views.py
+++ produtos/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Bebida, Pizza
+
 
 def render_index(request):
-    return render(request, 'index/home.html', {})
+    pizzas = Pizza.objects.all()
+    bebidas = Bebida.objects.all()
+
+    return render(request, 'index/home.html', {'pizzas': pizzas, 'bebidas': bebidas})

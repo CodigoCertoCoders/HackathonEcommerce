2d22ffe3af703c27c2194532418ba35ce0dc66d2
bentoluizv <bentoluizv@gmail.com>
2024-09-04 23:31:33 -0300

    add produtos to installed apps

--- pizzaria/settings.py
+++ pizzaria/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produtos',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'pizzaria.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database.db',
     }
 }
 

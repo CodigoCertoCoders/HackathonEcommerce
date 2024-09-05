4933029cc159dbe66bedd20088b26870b6ce49ad
bentoluizv <bentoluizv@gmail.com>
2024-09-05 10:11:25 -0300

    add description to products models

--- produtos/models.py
+++ produtos/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Pizza(models.Model):
     PizzaType = models.TextChoices('PizzaType', 'DOCE SALGADA')
     category = models.CharField(choices=PizzaType, max_length=10)  # type: ignore
-    name = models.CharField(max_length=10)
-    price = models.FloatField(max_length=10)
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    description = models.TextField(default='')
 
     def __str__(self):
         return self.name
 
 
 class Bebida(models.Model):
-    name = models.CharField(max_length=10)
-    price = models.FloatField(max_length=10)
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
 
     def __str__(self):
         return self.name

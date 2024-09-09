a9f7f198455cf1b38894d0217230516f9b8ed7f1
Julio Faria <juliofariadev@gmail.com>
2024-09-08 23:23:59 -0300

    feature: create user modal

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
     Cookies.remove('token');
     Cookies.remove('userId');
 
-    navigate('/login');
+    navigate('/');
   };
 
   useEffect(() => {
--- Frontend/src/pages/ProductPage.jsx
+++ Frontend/src/pages/ProductPage.jsx
@@ -1,46 +1,59 @@
-import React, { useContext, useState } from "react";
-import ProductCard from "../components/ProductComponents";
-import FilterComponents from "../components/FilterComponents";
-import { ProductContext } from "../context/ProductContext";
+import React, { useContext, useState } from 'react';
+import ProductCard from '../components/ProductComponents';
+import FilterComponents from '../components/FilterComponents';
+import { ProductContext } from '../context/ProductContext';
 import ShoppingCart from '../assets/shopping_cart.svg';
 import Person from '../assets/person.svg';
 import Search from '../assets/search.svg';
 import Adjustment from '../assets/adjustment.svg';
 import styles from './css/ProductPage.module.css';
-import productData from '../static/produtos.json'
-import { Link } from "react-router-dom";
+import productData from '../static/produtos.json';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
 
 const ProductPage = () => {
-  const { products,setProducts, activateFilter , setActivateFilter, activateMaisPedidos , setActivateMaisPedidos , cartProd , category , handleFilter} = useContext(ProductContext);
-  const [search , setSearch] = useState('')
+  const {
+    products,
+    setProducts,
+    activateFilter,
+    setActivateFilter,
+    activateMaisPedidos,
+    setActivateMaisPedidos,
+    cartProd,
+    category,
+    handleFilter,
+  } = useContext(ProductContext);
+  const [search, setSearch] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false); // Estado do modal
   const itemCount = cartProd.length;
-  // console.log(itemCount)
-  const handleSearch = (event) =>{
-    setSearch(event.target.value)
-  }
 
-  
-  const handleSubmit = (event) => {
-    if(search === ''){
-      setProducts(products)
-      console.log(search)
-    }else{
-      const filtered = products.filter(product =>
-        product.name.toLowerCase().includes(search.toLowerCase()) ||
-        product.category.toLowerCase().includes(search.toLowerCase())
-      );
-      setProducts(filtered)
-     
-      
-    }
+  const { logout } = React.useContext(AuthContext);
 
-    setSearch('') // Limpa o campo de pesquisa
-    setActivateMaisPedidos(false) // Volta a mostrar os produtos mais vendidos
-    event.preventDefault(); // Impede o recarregamento da página
-    
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    if (search === '') {
+      setProducts(products);
+    } else {
+      const filtered = products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(search.toLowerCase()) ||
+          product.category.toLowerCase().includes(search.toLowerCase()),
+      );
+      setProducts(filtered);
+    }
 
+    setSearch(''); // Limpa o campo de pesquisa
+    setActivateMaisPedidos(false); // Volta a mostrar os produtos mais vendidos
+    event.preventDefault(); // Impede o recarregamento da página
+  };
+
+  // Função para abrir/fechar o modal
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
@@ -50,21 +63,30 @@ const ProductPage = () => {
           <h3>UserName</h3>
         </div>
 
-        <div className={styles.mediaQuery}  style={{ position: 'relative', width: '250px', marginTop: '10px', marginBottom: '10px' }}>
-          <img src={Search}
+        <div
+          className={styles.mediaQuery}
+          style={{
+            position: 'relative',
+            width: '250px',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        >
+          <img
+            src={Search}
             style={{
               position: 'absolute',
               left: '5px',
               top: '45%',
               transform: 'translateY(-50%)',
-              color: "black"
+              color: 'black',
             }}
             alt="Ícone de busca"
           />
-          <form  className={styles.input}onSubmit={handleSubmit}>
+          <form className={styles.input} onSubmit={handleSubmit}>
             <input
               type="text"
-              placeholder="Esta procurando por..."
+              placeholder="Está procurando por..."
               value={search}
               onChange={handleSearch}
               style={{
@@ -74,107 +96,69 @@ const ProductPage = () => {
               }}
               className={styles.input}
             />
-           
           </form>
-          
-          <i
-            style={{
-              position: 'absolute',
-              right: '-20px',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              color: '#888',
-              cursor: 'pointer',
-            }}
-            className="fas fa-times"
-            
-          ></i>
-           <select className={styles.select} name="Filtre">
-              <option onClick={() => handleFilter('')}>Filtro</option>
-               {category.map((cat , index)=>(
-                <option onClick={() =>handleFilter(cat)}key={index} value={cat}>{cat}</option>
-               ))}
-    
-            </select>
-        </div>
 
+          <select className={styles.select} name="Filtre">
+            <option onClick={() => handleFilter('')}>Filtro</option>
+            {category.map((cat, index) => (
+              <option onClick={() => handleFilter(cat)} key={index} value={cat}>
+                {cat}
+              </option>
+            ))}
+          </select>
+        </div>
 
         <div>
           <div className={styles.cartIconContainer}>
-          <img className={styles.img} src={Person} alt="Ícone de pessoa" />
-          
-            <div className={styles.itemCount}>
+            {/* Ícone de usuário com clique para abrir modal */}
+            <img
+              className={styles.img}
+              src={Person}
+              alt="Ícone de pessoa"
+              onClick={toggleModal}
+              style={{ cursor: 'pointer' }}
+            />
 
+            <div className={styles.itemCount}>
               <p>{itemCount}</p>
             </div>
-            <Link to={"/cart"}> <img className={styles.img} src={ShoppingCart} alt="Carrinho de compras"/> </Link>
-          
+            <Link to={'/cart'}>
+              <img
+                className={styles.img}
+                src={ShoppingCart}
+                alt="Carrinho de compras"
+              />
+            </Link>
           </div>
         </div>
 
-
-
-      </section>
-      <section className={styles.containersearch}>
-        <div style={{ position: 'relative', width: '250px', marginTop: '10px', marginBottom: '10px' }}>
-          <img src={Search}
-            style={{
-              position: 'absolute',
-              left: '5px',
-              top: '45%',
-              transform: 'translateY(-50%)',
-              color: "black"
-            }}
-            alt="Ícone de busca"
-          />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Esta procurando por..."
-              value={search}
-              onChange={handleSearch}
-              style={{
-                width: '100%',
-                padding: '10px 10px 10px 30px',
-                border: '1px solid #ccc',
-              }}
-              className={styles.input}
-            />
-          </form>
-          
-          <i
-            style={{
-              position: 'absolute',
-              right: '-20px',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              color: '#888',
-              cursor: 'pointer',
-            }}
-            className="fas fa-times"
-          >
-          </i>
-        </div>
-        <img onClick={()=>{setActivateFilter(true) , setActivateMaisPedidos(false)}}className={styles.imgAdjustment} src={Adjustment} alt="Ícone de ajuste" />
-      </section>
-      <div className={`${''} ${activateMaisPedidos === true ? '' : styles.notFilterPage}`}>
-
-          <h3>Mais pedidos</h3>
-          <section className={styles.sectionMaisPedidos}>
-            <div className={styles.catalogProd}>
-              {products.slice(0,5).map((prod) => (
-                <Link to={`/catalog/${prod.uuid}`} className={styles.img}>
-                   <img key={prod.name} src={`https://maltex-back-production.up.railway.app/assets/${prod.url}`} className={styles.imgProd} alt={prod.name} />
-                </Link>
-               
-              ))}
+        {isModalOpen && (
+          <div className={styles.modalOverlay} onClick={toggleModal}>
+            <div
+              className={styles.modalContent}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <h4>Opções de Conta</h4>
+              <ul>
+                <li>
+                  <Link to="/adress">Endereço</Link>
+                </li>
+                <li>
+                  <button
+                    onClick={() => {
+                      logout();
+                    }}
+                  >
+                    Log Out
+                  </button>
+                </li>
+              </ul>
             </div>
-          </section>
-      </div>
-      
+          </div>
+        )}
+      </section>
 
       <h3>Todas as bebidas</h3>
-        
       <section className={styles.allProducts}>
         {products.map((prod) => (
           <ProductCard
@@ -185,13 +169,9 @@ const ProductPage = () => {
             id={prod.uuid}
           />
         ))}
-
       </section>
-      <div className={`${styles.filterPage} ${activateFilter === true ? styles.filterPage : styles.notFilterPage}`}>
-          <FilterComponents />
-        </div>
     </div>
   );
-}
+};
 
 export default ProductPage;

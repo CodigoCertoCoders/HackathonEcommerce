487dd5646092633a80d8d3dee5fe8dde9b32eeb7
Julio Faria <juliofariadev@gmail.com>
2024-09-08 22:46:53 -0300

    feat: add whatsapp payment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,7 +12,7 @@ import PublicRoute from './components/PublicRoute';
 import AdressPage from './pages/AdressPage';
 import CheckoutPage from './pages/CheckoutPage';
 import CartPage from './pages/CartPage';
-import { ToastContainer } from 'react-toastify'; 
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Importação do CSS do Toastify
 
 function App() {
@@ -21,14 +21,38 @@ function App() {
       <AuthProvider>
         <ProductProvider>
           <Routes>
-            <Route path="/" element={<PublicRoute element={<RegisterPage />} />} />
-            <Route path="/signup" element={<PublicRoute element={<SignUpPage />} />} />
-            <Route path="/signin" element={<PublicRoute element={<SignInPage />} />} />
-            <Route path="/catalog" element={<ProtectedRoute element={<ProductPage />} />} />
-            <Route path="/adress" element={<ProtectedRoute element={<AdressPage />} />} />
-            <Route path="/catalog/:id" element={<ProtectedRoute element={<ProductDetails />} />} />
-            <Route path="/checkout" element={<ProtectedRoute element={<CheckoutPage />} />} />
-            <Route path="/cart" element={<ProtectedRoute element={<CartPage />} />} />
+            <Route
+              path="/"
+              element={<PublicRoute element={<RegisterPage />} />}
+            />
+            <Route
+              path="/signup"
+              element={<PublicRoute element={<SignUpPage />} />}
+            />
+            <Route
+              path="/signin"
+              element={<PublicRoute element={<SignInPage />} />}
+            />
+            <Route
+              path="/catalog"
+              element={<ProtectedRoute element={<ProductPage />} />}
+            />
+            <Route
+              path="/adress"
+              element={<ProtectedRoute element={<AdressPage />} />}
+            />
+            <Route
+              path="/catalog/:id"
+              element={<ProtectedRoute element={<ProductDetails />} />}
+            />
+            <Route
+              path="/checkout"
+              element={<ProtectedRoute element={<CheckoutPage />} />}
+            />
+            <Route
+              path="/cart"
+              element={<ProtectedRoute element={<CartPage />} />}
+            />
           </Routes>
           {/* Adicionando o ToastContainer para que os toasts sejam exibidos */}
           <ToastContainer
--- Frontend/src/context/ProductContext.jsx
+++ Frontend/src/context/ProductContext.jsx
@@ -1,134 +1,137 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from 'react';
 
-export const ProductContext = createContext()
+export const ProductContext = createContext();
 
-export const ProductProvider = ({children})=>{
-    const [products , setProducts] = useState([])
-    const [productsData , setProductsData] = useState([])
-    const [productsFilter , setProductsFilter] = useState([])
-    const [activateFilter , setActivateFilter] = useState(false)
-    const [activateMaisPedidos , setActivateMaisPedidos] = useState(true)
-    const [category , setCategories] = useState([])
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [productsData, setProductsData] = useState([]);
+  const [productsFilter, setProductsFilter] = useState([]);
+  const [activateFilter, setActivateFilter] = useState(false);
+  const [activateMaisPedidos, setActivateMaisPedidos] = useState(true);
+  const [category, setCategories] = useState([]);
 
-    const [cartProd , setCartProd] = useState(() =>{
-      try {
-        // Tenta pegar o carrinho do localStorage
-        const savedCart = localStorage.getItem("cartProd");
-        
-        // Se existe e não é vazio, parseia, caso contrário, retorna um array vazio
-        return savedCart ? JSON.parse(savedCart) : [];
-      } catch (error) {
-        console.error("Erro ao carregar o carrinho do localStorage:", error);
-        return [];
-      }
-    })
+  const [cartProd, setCartProd] = useState(() => {
+    try {
+      // Tenta pegar o carrinho do localStorage
+      const savedCart = localStorage.getItem('cartProd');
 
-    console.log(products)
-    useEffect(() => {
-      //Aqui atualizo o carrinho sempre que o estado for atualizado
-      localStorage.setItem('cartProd' , JSON.stringify(cartProd));
-  }, [cartProd]);  
-
-
-
-    //CHAMADA DA API PARA OS PRODUTOS
-    useEffect(() => {
-      const url = "https://maltex-back-production.up.railway.app/products";
-      
-      fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then(response => response.json())
-      .then(data => {setProducts(data.products) , setProductsData(data.products)})
-      .catch(error => console.error('Erro:', error));
-      
-
-    }, []); 
-    
-    useEffect(()=>{
-      const uniqueCategory = [...new Set(productsData.map(prod => prod.category))]
-  
-      setCategories(uniqueCategory)
-  }, [products])
-  
-    
-
-    const handleFilter=(filter) =>{
-
-        if(filter === ''){
-            setProducts(productsData)
-        }else{
-            const filtered = products.filter(prod=>prod.category === filter)
-            console.log(filtered)
-            setProducts(filtered)
-        }
-
-       
+      // Se existe e não é vazio, parseia, caso contrário, retorna um array vazio
+      return savedCart ? JSON.parse(savedCart) : [];
+    } catch (error) {
+      console.error('Erro ao carregar o carrinho do localStorage:', error);
+      return [];
     }
+  });
 
-    const addToCart = (prod, qtd) => {
-        setCartProd((prevcartProd) => {
-          // Verifica se prevcartProd é um array
-          if (!Array.isArray(prevcartProd)) {
-            console.error('O estado prevcartProd não é um array:', prevcartProd);
-            return []; // Retorna um array vazio ou o estado anterior se necessário
-          }
-      
-          // Verifica se o produto já está no carrinho
-          const existItem = prevcartProd.find((item) => item.uuid === prod.id);
+  console.log(products);
+  useEffect(() => {
+    //Aqui atualizo o carrinho sempre que o estado for atualizado
+    localStorage.setItem('cartProd', JSON.stringify(cartProd));
+  }, [cartProd]);
 
-      
-          if (existItem) {
-            // Se o produto já está no carrinho, atualiza a quantidade
-            const novoCart = prevcartProd.map((item) =>
-              item.uuid === prod.id
-                ? { ...item, qtd: item.qtd + qtd }
-                : item
-            );
-      
-            // console.log("Atualizando produto:", novoCart);
-            return novoCart;
-          } else {
-            // Se o produto não está no carrinho, adiciona-o com a quantidade inicial
-            if(qtd === 0 ){
-              alert("Insira a quantidade do produto")
-            }else{
-              const result = { ...prod, qtd: qtd };
-              // console.log("Adicionando novo produto:", cartProd);
-              return [...prevcartProd, result];
-            }
-            
-          }
-        });
-      };
-      //Função para limpar o carrinho de uma vez
-      const removeCart = () =>{
-        setCartProd([])
-        localStorage.removeItem('cartProd')
+  //CHAMADA DA API PARA OS PRODUTOS
+  useEffect(() => {
+    const url = 'https://maltex-back-production.up.railway.app/products';
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products), setProductsData(data.products);
+      })
+      .catch((error) => console.error('Erro:', error));
+  }, []);
+
+  useEffect(() => {
+    const uniqueCategory = [
+      ...new Set(productsData.map((prod) => prod.category)),
+    ];
+
+    setCategories(uniqueCategory);
+  }, [products]);
+
+  const handleFilter = (filter) => {
+    if (filter === '') {
+      setProducts(productsData);
+    } else {
+      const filtered = products.filter((prod) => prod.category === filter);
+      console.log(filtered);
+      setProducts(filtered);
+    }
+  };
+
+  const addToCart = (prod, qtd) => {
+    setCartProd((prevcartProd) => {
+      // Verifica se prevcartProd é um array
+      if (!Array.isArray(prevcartProd)) {
+        console.error('O estado prevcartProd não é um array:', prevcartProd);
+        return []; // Retorna um array vazio ou o estado anterior se necessário
       }
 
-      const updateQtd = (prodId, qtd) =>{
-       
-        setCartProd((prevCart)=>{
-          const update = prevCart.map((item)=>(
-            item.uuid === prodId ? {...item , 'qtd' : qtd} : item
-            
-          )) .filter((item) => item.qtd > 0);
+      // Verifica se o produto já está no carrinho
+      const existItem = prevcartProd.find((item) => item.uuid === prod.id);
 
-          localStorage.setItem('cartProd' , JSON.stringify(update))
-          console.log("Atualizou o valor", update)
-          return update
-        })
+      if (existItem) {
+        // Se o produto já está no carrinho, atualiza a quantidade
+        const novoCart = prevcartProd.map((item) =>
+          item.uuid === prod.id ? { ...item, qtd: item.qtd + qtd } : item,
+        );
+
+        // console.log("Atualizando produto:", novoCart);
+        return novoCart;
+      } else {
+        // Se o produto não está no carrinho, adiciona-o com a quantidade inicial
+        if (qtd === 0) {
+          alert('Insira a quantidade do produto');
+        } else {
+          const result = { ...prod, qtd: qtd };
+          // console.log("Adicionando novo produto:", cartProd);
+          return [...prevcartProd, result];
+        }
       }
-      
-      
+    });
+  };
+  //Função para limpar o carrinho de uma vez
+  const removeCart = () => {
+    setCartProd([]);
+    localStorage.removeItem('cartProd');
+  };
 
-    return(
-        <ProductContext.Provider value={{products,setProducts , productsFilter , setProductsFilter , handleFilter , activateFilter , setActivateFilter, addToCart, activateMaisPedidos , category, setActivateMaisPedidos , cartProd , updateQtd}}>
-            {children}
-        </ProductContext.Provider>
-    )
-} 
+  const updateQtd = (prodId, qtd) => {
+    setCartProd((prevCart) => {
+      const update = prevCart
+        .map((item) => (item.uuid === prodId ? { ...item, qtd: qtd } : item))
+        .filter((item) => item.qtd > 0);
+
+      localStorage.setItem('cartProd', JSON.stringify(update));
+      console.log('Atualizou o valor', update);
+      return update;
+    });
+  };
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        setProducts,
+        productsFilter,
+        setProductsFilter,
+        handleFilter,
+        activateFilter,
+        setActivateFilter,
+        addToCart,
+        activateMaisPedidos,
+        category,
+        setActivateMaisPedidos,
+        cartProd,
+        updateQtd,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
